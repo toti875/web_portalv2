947ac883f26399405994a0dc5a93dbb66b67a552
Jeferson Martin de Araujo <jmartin20k@gmail.com>
2022-10-06 01:44:26 -0300

    site

--- src/assets/images/ChevronIcon.tsx
+++ src/assets/images/ChevronIcon.tsx
@@ -9,3 +9,4 @@ export const ChevronIcon: React.FC<ChevronIconProps> = (props: ChevronIconProps)
 		<path d="M1.41 0.590088L6 5.17009L10.59 0.590088L12 2.00009L6 8.00009L0 2.00009L1.41 0.590088Z" fill="var(--icons)" />
 	</svg>
 );
+
