6a2456666c2905b2a2c5da55d896a1909cf881e8
Jeferson Martin de Araujo <jmartin20k@gmail.com>
2022-10-20 09:58:58 -0300

    fortem1

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,19 +2,20 @@ import { DEFAULT_KYC_STEPS, ORDER_BOOK_DEFAULT_SIDE_LIMIT, STORAGE_DEFAULT_LIMIT
 
 export const defaultConfig: Config = {
 	api: {
-		authzURL: 'https://www.fortem.website/api/v2/barong',
-		switchURL: 'https://www.fortem.website/api/v2/applogic',
-		transactionURL: 'https://WWW.fortem.website/api/v2/peatio',
-		ieoURL: 'https://www.fortem.website/api/v2/peatio',
-		infoURL: 'https://www.fortem.website/api/v2/peatio',
+		authzURL: 'https://www.fortem1.com.br/api/v2/barong',
+		switchURL: 'https://www.fortem1.com.br/api/v2/applogic',
+		transactionURL: 'https://WWW.fortem1.com.br/api/v2/peatio',
+		ieoURL: 'https://www.fortem1.com.br/api/v2/peatio',
+		infoURL: 'https://www.fortem1.com.br/api/v2/peatio',
 
 
 		//downstreamURL: 'ws://demo.fortem-financial.io/api/v2/downstream',
-		downstreamURL: 'wss://www.fortem.website/api/v2/ranger',
+		//downstreamURL: 'localhost:3002/api',
+		downstreamURL: 'wss://fortem1.com.br/api/v2/ranger' 
 	},
 	minutesUntilAutoLogout: '5',
 	rangerReconnectPeriod: '1', 
-	withCredentials: true,
+	withCredentials: false,
 	storage: {},
 	gaTrackerKey: 'G-M47QL88D4R',
 	msAlertDisplayTime: '10000',
