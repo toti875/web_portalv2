0974fd9e185247d0fed375602784c2148a63e27d
Jeferson Martin de Araujo <jmartin20k@gmail.com>
2022-11-02 18:04:28 -0300

    launch

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import { DEFAULT_KYC_STEPS, ORDER_BOOK_DEFAULT_SIDE_LIMIT, STORAGE_DEFAULT_LIMIT } from '../constants';
 
 export const defaultConfig: Config = {
-	
+	/*
 		api: {
 		authzURL: 'http://localhost:9002/api/v2/barong',
 		switchURL: 'http://localhost:9002/v2/applogic',
@@ -11,8 +11,9 @@ export const defaultConfig: Config = {
 		infoURL: 'http://local.fortem-financia.io:9002/api/v2/peatio',
 		downstreamURL: 'ws://local.fortem-financia.io:9003/api/public/',
 	},
+	*/
 
-	/*
+	
 	api: {
 		authzURL: 'https://www.fortem1.com.br/api/v2/barong',
 		switchURL: 'https://www.fortem1.com.br/api/v2/applogic',
@@ -22,7 +23,7 @@ export const defaultConfig: Config = {
 		infoURL: 'https://www.fortem1.com.br/api/v2/peatio',
 		downstreamURL: 'wss://www.fortem1.com.br/api/public/',
 	},
-	*/
+	
 	minutesUntilAutoLogout: '2',
 	rangerReconnectPeriod: '1', 
 	withCredentials: false,
