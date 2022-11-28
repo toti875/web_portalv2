7b8a3e60319c43d31b79664db77658ddaa7b7e90
Jeferson Martin de Araujo <jmartin20k@gmail.com>
2022-11-28 16:59:48 -0300

    v1

--- src/api/config.ts
+++ src/api/config.ts
@@ -15,14 +15,14 @@ export const defaultConfig: Config = {
 
 	
 	api: {
-		authzURL: 'http://www.fortem1.com.br/api/v2/barong',
-		switchURL: 'http://www.fortem1.com.br/api/v2/applogic',
-		transactionURL: 'http://www.fortem1.com.br/api/v2/peatio',
-		ieoURL: 'http://www.fortem1.com.br/api/v2/peatio',
+		authzURL: 'https://www.fortem1.com.br/api/v2/barong',
+		switchURL: 'https://www.fortem1.com.br/api/v2/applogic',
+		transactionURL: 'https://www.fortem1.com.br/api/v2/peatio',
+		ieoURL: 'https://www.fortem1.com.br/api/v2/peatio',
 
-		infoURL: 'http://www.fortem1.com.br/api/v2/peatio',
+		infoURL: 'https://www.fortem1.com.br/api/v2/peatio',
 		//downstreamURL: 'wss://www.fortem1.com.br/api/public/',
-		downstreamURL: 'ws://www.fortem1.com.br/api/v2/ranger',
+		downstreamURL: 'wss://www.fortem1.com.br/api/v2/ranger',
 
 	},
 	
@@ -34,14 +34,14 @@ export const defaultConfig: Config = {
 	gaTrackerKey: 'G-M47QL88D4R',
 	msAlertDisplayTime: '10000',
 	msPricesUpdates: '3000',
-	incrementalOrderBook: true,
+	incrementalOrderBook: false,
 	finex: false,
 	isResizable: true,
 	isDraggable: true,
 	languages: ['pt'],
 	sessionCheckInterval: '1500000',
 	balancesFetchInterval: '1500000',
-	passwordEntropyStep: 7,
+	passwordEntropyStep: 14,
 	showLanding: true,
 	sentryEnabled: false,
 
